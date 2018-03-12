5a3508447b19dd43c343ed684a2dacd0ecb7c41a
jack <yang971460565@163.com>
2018-03-12 17:26:48 +0800

    回家看开源项目 Fragment

---
+++ app/src/main/java/com/example/yw/action/view/fragment/FragmentDemo.java
@@ -0,0 +1,37 @@
+package com.example.yw.action.view.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jack
+ * On 18-3-12:下午2:18
+ * Desc:
+ */
+
+public class FragmentDemo extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        testTransaction();
+    }
+
+    private void testTransaction() {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(1, new FragmentDemo());
+    }
+}

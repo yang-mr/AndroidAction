8eade3aef80b1675e01696df3cd4621a926a5cf4
jack <yang971460565@163.com>
2018-03-27 14:11:34 +0800

    Add factory mode demo

---
+++ app/src/main/java/com/example/yw/action/java/design_mode/factory/abstract_factory_mode/BenchiClutch.java
@@ -0,0 +1,14 @@
+package com.example.yw.action.java.design_mode.factory.abstract_factory_mode;
+
+/**
+ * Created by jack
+ * On 18-3-27:下午12:03
+ * Desc:
+ */
+
+public class BenchiClutch implements Clutch {
+    @Override
+    public void press() {
+        System.out.println("benchi clutch press....");
+    }
+}
---
+++ app/src/main/java/com/example/yw/action/java/design_mode/factory/abstract_factory_mode/Car.java
@@ -0,0 +1,35 @@
+package com.example.yw.action.java.design_mode.factory.abstract_factory_mode;
+
+/**
+ * Created by jack
+ * On 18-3-27:上午11:58
+ * Desc:
+ */
+
+public class Car {
+    private Engine engine;
+    private Clutch clutch;
+    private AbsFactory factory;
+
+    public Car(Engine engine) {
+        this.engine = engine;
+    }
+
+    public Car(Engine engine, Clutch clutch) {
+        this.engine = engine;
+        this.clutch = clutch;
+    }
+
+    public Car(AbsFactory factory) {
+        this.factory = factory;
+    }
+
+    public void start() {
+        if (factory != null) {
+            this.engine = factory.createEngine();
+            this.clutch = factory.createClutch();
+        }
+        engine.start();
+        clutch.press();
+    }
+}

7f8272963ea8de54b026b2a72ded22239dcc6efe
jack <yang971460565@163.com>
2018-03-30 13:57:41 +0800

    Add proxy mode

---
+++ app/src/main/java/com/example/yw/action/java/design_mode/proxy/Origin.java
@@ -0,0 +1,13 @@
+package com.example.yw.action.java.design_mode.proxy;
+
+/**
+ * Created by jack
+ * On 18-3-30:下午1:51
+ * Desc:
+ */
+
+public class Origin implements IOrigin {
+    public void exe() {
+        System.out.println("my name is origin...");
+    }
+}

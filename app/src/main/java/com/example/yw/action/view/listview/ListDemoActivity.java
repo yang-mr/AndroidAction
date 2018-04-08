175910e941a71cf2667a756c6b82ad45a87df733
yangwei <yang971460565@126.com>
2018-04-08 23:32:08 +0800

    modify listView

--- app/src/main/java/com/example/yw/action/view/listview/ListDemoActivity.java
+++ app/src/main/java/com/example/yw/action/view/listview/ListDemoActivity.java
@@ -2,17 +2,61 @@ package com.example.yw.action.view.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 import com.example.yw.action.R;
+
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * ref :
  * https://blog.csdn.net/iispring/article/details/50967445
  */
 public class ListDemoActivity extends AppCompatActivity {
-
+    private ListView mListView;
+    private MyListAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_demo);
+
+        mListView = findViewById(R.id.listview);
+        mAdapter = new MyListAdapter(Arrays.asList("111", "2222"));
+        mListView.setAdapter(mAdapter);
+
+        mAdapter.notifyDataSetChanged();
+    }
+
+    class MyListAdapter extends BaseAdapter {
+        private List<String> data;
+
+        public MyListAdapter(List<String> data) {
+            this.data = data;
+        }
+
+        @Override
+        public int getCount() {
+            return data.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return data.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // to do...
+            return null;
+        }
     }
 }
